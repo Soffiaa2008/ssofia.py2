065476aaa43aeed67b1874c2334428cb78268f79
софия <bisssofia06@gmail.com>
2022-11-15 19:42:02 +0300

    сделали рандом и таймер

--- guessWord.py
+++ guessWord.py
@@ -1,7 +1,24 @@
+from time import sleep 
+from random import choice
+
+def timer (n):
+    for i in range(n,-1,-1):
+        sleep(1)
+        print(i)
+
+
+words =["яблоко","персик","банан","клубника"]
 while True:
-    word= "яблоко"
-    print("угадай слово : я****о . Подсказка->это фрукт")
+    word= choice(words)
+    first=word[0]
+    last=word[len(word)-1]
+    print("угадай слово")
+    print("первая буква-"+first)
+    print("последняя буква-"+last)
+    print("длина слова-"+str(len(word)))
+    timer(5)
     ans=input("ответ->")
+    timer(3)
     if word==ans:
         print("отлично,ты угадал")
         break
