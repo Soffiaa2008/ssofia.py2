82ca57f35999307757f2156cf1de52559a587d84
софия <bisssofia06@gmail.com>
2022-11-17 19:47:48 +0300

    добавлен вопрос какие планы?

--- bot.py
+++ bot.py
@@ -2,10 +2,10 @@ import random
 
 greetings=["Привет","Здравствуй","Приветствую"]
 affairs=["отлично","хорошо","нормально","плохо","так себе","пойдет"]
-
+plans=["поесть","поспать","никаких","мне лень что либо делать)","прогуляться"]
 isHello=False
 isAffairs=False
-
+isPlans=False
 while True:
 
     message=input("я:").lower()
@@ -17,7 +17,8 @@ while True:
         else:
             print("бот:уже здоровались!")
         
-
+    if  not isHello :
+        print("бот:а поздоророваться:(")
 
     if message =="как дела?" and isHello:
         if not isAffairs:
@@ -25,8 +26,18 @@ while True:
             isAffairs=True
         else:
             print("бот:уже спрашивал!")
-    elif  not isHello   :
-        print("бот:а поздоророваться:(")
+
+
+
+
+    if message =="какие планы?" and isHello:
+        if not isPlans:
+            print("бот: "+ random.choice(plans))
+            isPlans=True
+        else:
+            print("бот: я уже говорил тебе!")
+
+
 
 
 
