30267f34cda87d388f5cf9d6d4bd4c8b2c58cd98
софия <bisssofia06@gmail.com>
2022-11-17 19:36:13 +0300

    добавлены все дополнительные функции

--- bot.py
+++ bot.py
@@ -3,14 +3,30 @@ import random
 greetings=["Привет","Здравствуй","Приветствую"]
 affairs=["отлично","хорошо","нормально","плохо","так себе","пойдет"]
 
+isHello=False
+isAffairs=False
+
 while True:
 
     message=input("я:").lower()
     
     if message =="привет":
-        print("бот: "+ random.choice(greetings))
-    elif message =="как дела?":
-        print("бот: "+ random.choice(affairs))
+        if  not isHello:
+            print("бот: "+ random.choice(greetings))
+            isHello=True
+        else:
+            print("бот:уже здоровались!")
+        
+
+
+    if message =="как дела?" and isHello:
+        if not isAffairs:
+            print("бот: "+ random.choice(affairs))
+            isAffairs=True
+        else:
+            print("бот:уже спрашивал!")
+    elif  not isHello   :
+        print("бот:а поздоророваться:(")
 
 
 
