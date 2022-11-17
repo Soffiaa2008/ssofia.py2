7c19f256962872b497e31135ae8e19a99b923786
софия <bisssofia06@gmail.com>
2022-11-17 19:07:24 +0300

    cоздаём чат бота

---
+++ bot.py
@@ -0,0 +1,24 @@
+import random
+
+greetings=["Привет","Здравствуй","Приветствую"]
+
+message=input("я:").lower()
+
+if message =="привет":
+    print("бот: "+ random.choice(greetings))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
