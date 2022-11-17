aa35d020f2e592aa678531dda088bfa420298487
софия <bisssofia06@gmail.com>
2022-11-17 19:14:36 +0300

    добавлены вопрос как дела и приветствие

--- bot.py
+++ bot.py
@@ -1,11 +1,16 @@
 import random
 
 greetings=["Привет","Здравствуй","Приветствую"]
+affairs=["отлично","хорошо","нормально","плохо","так себе","пойдет"]
 
-message=input("я:").lower()
+while True:
 
-if message =="привет":
-    print("бот: "+ random.choice(greetings))
+    message=input("я:").lower()
+    
+    if message =="привет":
+        print("бот: "+ random.choice(greetings))
+    elif message =="как дела?":
+        print("бот: "+ random.choice(affairs))
 
 
 
